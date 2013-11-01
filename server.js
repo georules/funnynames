7ba1e539ba7233b9bc4014ddf388839a4517bb46
Geo Miller <geoffery.miller@gmail.com>
2013-11-01 18:23:53 -0400

    production credentials?

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var settings = require('./settings')
 var port = settings.port || 8080;
-var env = settings.env || "dev";
+var env = settings.env || "development";
+var mongo = require('mongoskin')
+var db = mongo.db(settings.mongo)
+
+var names = db.collection("names")
+var hits = db.collection("hits")
 
 var express = require('express')
 var app = express()
@@ -13,7 +18,10 @@ function allow(itall) {
 
 app.get('/', function(req,res) {
   allow(res)
-  res.send("hello from " + env + "!")
+  hits.save({}, function (e,r) { if (e) console.log(e)})
+  hits.count({}, function (e,r) {
+    res.send("hello from " + env + "!"+"<br>"+r)
+  })
 });
 
 app.use(express.favicon('favicon.ico'));
--- settings.js
+++ settings.js
@@ -1,5 +1,18 @@
-
-module.exports = {
+var config = {
   port: 8080,
-  env: process.env.NODE_ENV
+  env: process.env.NODE_ENV,
+  mongo: "mongodb://localhost:27017/names"
 }
+ /* mongo: {
+    user:"", pass:"",server:"localhost",
+    port:27017,db:"names"
+  }
+*/
+
+if (config.env === 'production')  {
+  user = process.env.MONGO_USER
+  pass = process.env.MONGO_PASS
+  config.mongo = "mongodb://"+user+":"+pass+"@ds045988.mongolab.com:45988/nodejitsu_georules_nodejitsudb4637748226"
+}
+
+module.exports = config
