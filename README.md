4f6cebcc41a5a74baaca9ec08581072578609dc7
Geo Miller <geoffery.miller@gmail.com>
2013-11-01 16:59:11 -0400

    first

---
+++ README.md
@@ -0,0 +1,7 @@
+funnynames api
+======
+
+[![Nodejitsu Deploy Status Badges](https://webhooks.nodejitsu.com/georules/funnynames.png)]
+(https://webops.nodejitsu.com#georules/webhooks)
+
+Hosted at http://georules-funnynames.jit.su/
---
+++ server.js
@@ -0,0 +1,19 @@
+var settings = require('./settings')
+var express = require('express')
+var app = express().use(express.bodyParser())
+
+function allow(itall) {
+  itall.header("Access-Control-Allow-Origin","*")
+}
+
+app.get('/', function(req,res) {
+  allow(res)
+  res.send("hello!")
+});
+
+app.use(express.favicon('favicon.ico'));
+//app.use(pageNotFound);
+
+app.listen(settings.port, function() {
+  console.log('Listening on port ' + settings.port)
+})
