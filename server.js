797593251269aa9c064f4e255f94699dbbfcc287
Geo Miller <geoffery.miller@gmail.com>
2014-10-27 22:01:09 -0400

    just some stuff

--- server.js
+++ server.js
@@ -3,11 +3,9 @@ var port = settings.port || 8080;
 var env = settings.env || "development";
 var mongo = require('mongoskin')
 var db = mongo.db(settings.mongo)
+db.bind('names')
 
-var util = require('util')
-
-var names = db.collection("names")
-var hits = db.collection("hits")
+var S = require('string');
 
 var express = require('express')
 var app = express()
@@ -21,7 +19,6 @@ function allow(itall) {
 }
 
 function mainWeb(req,res) {
-  allow(res)
   res.sendfile('./static/index.html')
 }
 
@@ -29,24 +26,29 @@ app.get('/', mainWeb)
 
 app.post('/', function(req,res) {
   allow(res)
-  name = req.body.datname
-  names.save({name: name}, mainWeb(req,res))   
+  name = S(req.body.datname).stripTags().s
+  db.names.save({name: name}, mainWeb(req,res))   
 })
 
 app.get('/api', function(req,res) {
   allow(res)
-  names.count({}, function (e,r) {
+  db.names.count({}, function (e,r) {
     numnames=r
-    pick = Math.floor(Math.random()*numnames)
-    names.find({},{limit:1,skip:pick}).toArray(function (e,r)  {
-      if (r.length > 0) res.send(r[0])
-      else  res.send("Scruffy")
-    })
+    if(numnames === 0) {
+      res.send("Scruffy")
+    }
+    else {
+      pick = Math.floor(Math.random()*numnames)
+      db.names.find().limit(-1).skip(pick).toArray(function (e,r)  {
+        if (r.length > 0) res.send(r[0])
+        else  res.send("Scruffy")
+      })
+    }
   })
 })
 
 app.get('/api/all', function(req,res)  {
-  names.find().toArray(function(e,r)  {
+  db.names.find().toArray(function(e,r)  {
     res.send(r)
   })
 })
